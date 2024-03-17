05c8f94df2045cd510fc6c534a1ec64210edb82b
EnderHonda <119761553+Endersonn@users.noreply.github.com>
2024-03-17 16:07:19 -0400

    Segundo comit num2
    en esta parte cree el menu del juego con un repeat para que se repita infinitamente hasta que la persona le de a el 4 para salir el programa

--- BuscaMinitas.pas
+++ BuscaMinitas.pas
@@ -1,3 +1,45 @@
 program BuscaMinitas;
+var
+  modo: integer;
 begin
+repeat
+  writeln('[][][][][][][][][][][][][][][][][][]');
+  writeln('[]                                []');
+  writeln('[]      Elige el modo de juego    []');
+  writeln('[]                                []');
+  writeln('[][][][][][][][][][][][][][][][][][]');
+  writeln('[]                                []');
+  writeln('[] 1. Facil (8x8 con 10 minas)    []');
+  writeln('[]                                []');
+  writeln('[] 2. Medio (16x16 con 40 minas)  []');
+  writeln('[]                                []');
+  writeln('[] 3. Dificil (16x30 con 99 minas)[]');
+  writeln('[]                                []');
+  writeln('[] 4. Salir                       []');
+  writeln('[][][][][][][][][][][][][][][][][][]');
+  readln(modo);
+
+  case modo of
+    1: begin 
+   
+    end;
+    
+    
+    2: begin 
+   
+    end;
+    3: begin
+ 
+    end;
+    4: begin 
+	writeln('GRACIAS POR JUGARLO ;)');
+    
+    exit; 
+    
+    end;
+  else
+    writeln('Modo no valido');
+  end;
+until modo = 4;
+  
 end.
